0f5e49b032e5cd14e6a838572256c3828d8a0cef
Dsnon <danielboydewoo@gmail.com>
2025-11-29 01:32:05 -0800

    Update DialogueModule.lua

--- DialogueModule.lua
+++ DialogueModule.lua
@@ -1,16 +1,77 @@
-local Dialogue = {}
+local DialogueModule = {}
 
-Dialogue.Lines = {
-	Happy = {"Nice to see you!", "You're fun to be around!"},
-	Scared = {"Wh-what was that?!", "I don't feel safe..."},
-	Annoyed = {"Can you not?", "Back off, please."},
-	Curious = {"What's that you're doing?", "Interesting move!"},
+DialogueModule.Lines = {
+	Happy = {
+		"Nice day, right?",
+		"I'm feeling great today!",
+		"Lovely weather we're having.",
+	},
+
+	Annoyed = {
+		"Ugh, seriously?",
+		"Not in the mood right now.",
+		"Can you give me space?",
+	},
+
+	Scared = {
+		"What was THAT?",
+		"Did you hear something?",
+		"I don't like this...",
+	},
+
+	Curious = {
+		"Hmm… interesting.",
+		"What's that over there?",
+		"You seem busy!",
+	},
+
+	Neutral = {
+		"Hello.",
+		"How are you?",
+		"Nice seeing you.",
+	},
 }
 
-function Dialogue:GetLine(emotion)
-	local pool = self.Lines[emotion]
-	if not pool then return "..." end
-	return pool[math.random(1, #pool)]
+DialogueModule.Responses = {
+	Happy = {
+		"Absolutely!",
+		"Couldn't agree more!",
+		"Yeah, it’s great!",
+	},
+
+	Annoyed = {
+		"Chill out.",
+		"Alright, alright.",
+		"Okay then…",
+	},
+
+	Scared = {
+		"Calm down, it's fine.",
+		"I don’t think it was anything.",
+		"Yikes… maybe.",
+	},
+
+	Curious = {
+		"Oh really?",
+		"What makes you say that?",
+		"Interesting thought!",
+	},
+
+	Neutral = {
+		"Yep.",
+		"Sure.",
+		"Right.",
+	},
+}
+
+function DialogueModule:GetLine(emotion)
+	local list = self.Lines[emotion]
+	return list[math.random(1, #list)]
 end
 
-return Dialogue
+function DialogueModule:GetResponse(emotion)
+	local list = self.Responses[emotion]
+	return list[math.random(1, #list)]
+end
+
+return DialogueModule
