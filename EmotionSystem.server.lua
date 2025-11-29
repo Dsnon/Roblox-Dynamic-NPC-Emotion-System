0ec92f08f22a5ad8213aea653cf979a2c3428abb
Dsnon <danielboydewoo@gmail.com>
2025-11-29 01:23:39 -0800

    Update EmotionSystem.server.lua
    Now using personalities + mood blending

--- EmotionSystem.server.lua
+++ EmotionSystem.server.lua
@@ -1,27 +1,28 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
-local EmotionEvents = ReplicatedStorage:WaitForChild("EmotionEvents")
-local EmotionModule = require(ReplicatedStorage:WaitForChild("EmotionModule"))
 
-local updateEvent = EmotionEvents:WaitForChild("UpdateEmotion")
+local Events = ReplicatedStorage:WaitForChild("EmotionEvents")
+local EmotionController = require(ReplicatedStorage:WaitForChild("EmotionController"))
+local DialogueModule = require(script.Parent.DialogueModule)
+local NPCConfig = require(ReplicatedStorage.NPCConfig)
 
---Trigger the emotions from player actions 
+local updateEvent = Events:WaitForChild("UpdateEmotion")
+
 Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(char)
-        local humanoid = char:WaitForChild("Humanoid")
+	player.CharacterAdded:Connect(function(char)
+		local hum = char:WaitForChild("Humanoid")
 
-        humanoid.StateChanged:Connect(function(_, new)
-            if new == Enum.HumanoidStateType.Jumping then
-              updateEvent:FireAllClients(char, "Curious")
-            end
-          end)
-      end)
-  end)
+		hum.StateChanged:Connect(function(_, new)
+			if new == Enum.HumanoidStateType.Jumping then
+				updateEvent:FireAllClients(char, "Curious")
+			end
+		end)
+	end)
 end)
 
--- Explosion > Scared NPCs
 workspace.ChildAdded:Connect(function(obj)
-  if obj:IsA("Explosion") then
-    updateEvent:FireALlClients(obj, "Scared")
-  end
+	if obj:IsA("Explosion") then
+		updateEvent:FireAllClients(obj, "Scared")
+	end
 end)
+
